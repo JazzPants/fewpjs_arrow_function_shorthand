5abb84b6a1f284dce55bc5202e38dc65da7aa1cf
JazzPants <leighton.l14@gmail.com>
2021-11-08 15:12:18 +1100

    Done!

---
+++ index.js
@@ -0,0 +1,19 @@
+let divide = () => 2000/100 //no arguments, divide just does one operation: 2000 / 100
+//like function divide() { return 2000/100 }
+//let divide = (x, y) => x/y;
+
+console.log(divide())
+
+let square = x => x ** 2;
+
+console.log(square(3))
+
+let add = (x, y) => x + y;
+
+console.log(add(5,3))
+
+function test() {
+    return 2000/100
+}
+
+console.log(test())
